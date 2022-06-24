7ee0179bbbaaaf281c7295fbfba81b2e29f6d945
temich719 <temich719@mail.ru>
2022-06-24 13:52:37 +0300

    dto refactored

--- Service/src/test/java/service/UserServiceImplTest.java
+++ Service/src/test/java/service/UserServiceImplTest.java
@@ -3,7 +3,7 @@ package service;
 import com.epam.esm.dao.UserDAO;
 import com.epam.esm.domain.Order;
 import com.epam.esm.domain.User;
-import com.epam.esm.dtos.*;
+import com.epam.esm.dto.*;
 import com.epam.esm.exception.RepositoryException;
 import com.epam.esm.util.Mapper;
 import org.junit.jupiter.api.BeforeEach;
